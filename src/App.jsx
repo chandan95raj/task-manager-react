0165284f8d18f30f7e315bf7d7b02adda42263a4
chandan <chandanraaz91@gmail.com>
2024-06-02 13:45:59 +0530

    tast

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <>
       <h1 class="text-3xl font-bold text-center">
-        Hello world! Task Manager
+        Hello world! Task Manager | Test
       </h1>
     </>
   )
